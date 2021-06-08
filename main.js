bdf37101bf213814120c71f4e4add31acdee9a2b
M3htbD301 <81451074+M3htbD301@users.noreply.github.com>
2021-06-08 16:55:05 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-var fullName=firstName.concat(lastName);
-console.log(fullName);
 var firstName="Mehtab ";
 console.log(firstName);
 var lastName="Deol";
 console.log(lastName);
+var fullName=firstName.concat(lastName);
+console.log(fullName);
 var greeting=fullName.replace("Deol","says hi");
 console.log(greeting);
